f24e18a2b56f13d17c6b5097ccaebd0415b0a189
İlker <36369727+ilkerkanber@users.noreply.github.com>
2021-09-08 22:38:17 +0300

    edit

--- UnityRules/Assets/Kodlar/StaticClassMantıgı/Class.cs
+++ UnityRules/Assets/Kodlar/StaticClassMantıgı/Class.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 public class Class
 {
     public int ID { get; set; }
-    public string Name  { get; set; }
-    public decimal Price{ get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
 
     public void NormalMethod()
     {
-
+        Debug.Log(ClassStatic.GetMessage());
     }
-
-    // 
 }
